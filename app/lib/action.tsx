29869fd412cc03a000aee43397247cfb4f980446
zikun-w <zikun@stu.xjtu.edu.cn>
2025-06-10 19:05:10 +0800

    day 6

--- app/lib/action.tsx
+++ app/lib/action.tsx
@@ -4,27 +4,52 @@ import { z } from 'zod';
 import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
 import postgres from 'postgres';
+import { signIn } from '@/auth';
+import { AuthError } from 'next-auth';
 
 const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
 
 const FormSchema = z.object({
   id: z.string(),
-  customerId: z.string(),
-  amount: z.coerce.number(),
-  status: z.enum(['pending', 'paid']),
+  customerId: z.string({
+    invalid_type_error: 'Please select a customer',
+  }),
+  amount: z.coerce.number().gt(
+    0, { message: 'Please enter an amount greater than $0' },
+  ),
+  status: z.enum(['pending', 'paid'], {
+    invalid_type_error: 'Please select an invoice status',
+  }),
   date: z.string(),
 })
+
+export type State = {
+  errors?: {
+    customerId?: string[];
+    amount?: string[];
+    status?: string[];
+  };
+  message?: string | null;
+};
 
 const CreateInvoices = FormSchema.omit({ id: true, date: true });
 const UpdateInvoices = FormSchema.omit({ id: true, date: true });
 
-export async function createInvoices(formData: FormData) {
-  const { customerId, amount, status } = CreateInvoices.parse({
+export async function createInvoices(prevState: State, formData: FormData) {
+  const validatedFields = CreateInvoices.safeParse({
     customerId: formData.get('customerId'),
     amount: formData.get('amount'),
     status: formData.get('status'),
   });
 
+  if (!validatedFields.success) {
+    return {
+      errors: validatedFields.error.flatten().fieldErrors,
+      message: 'Missing Fields. Failed to Create Invoice',
+    };
+  }
+
+  const { customerId, amount, status } = validatedFields.data;
   const amountInCents = Math.round(amount * 100);
   const date = new Date().toISOString().split('T')[0];
 
@@ -74,4 +99,23 @@ export async function deleteInvoice(id: string) {
   `;
 
   revalidatePath('/dashboard/invoices');
+}
+
+export async function authenticate(
+  prevState: string | undefined,
+  formData: FormData,
+) {
+  try {
+    await signIn('credentials', formData);
+  } catch (error) {
+    if (error instanceof AuthError) {
+      switch (error.type) {
+        case 'CredentialsSignin':
+          return 'Invalid credentials.';
+        default:
+          return 'Something went wrong.';
+      }
+    }
+    throw error;
+  }
 }
